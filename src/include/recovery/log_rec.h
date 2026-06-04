42ff5355fb4bfce3af4e7b78260755c1c69f90f9
Nyaaa <3240106167@zju.edu.cn>
2026-06-04 16:32:04 +0800

    finish recovery manager
    通过recovery_manager_test

--- src/include/recovery/log_rec.h
+++ src/include/recovery/log_rec.h
@@ -23,15 +23,30 @@ using KeyType = std::string;
 using ValType = int32_t;
 
 /**
- * TODO: Student Implement
+ * DONE: Student Implement
  */
 struct LogRec {
     LogRec() = default;
-
+ 
     LogRecType type_{LogRecType::kInvalid};
-    lsn_t lsn_{INVALID_LSN};
-    lsn_t prev_lsn_{INVALID_LSN};
-
+    lsn_t      lsn_{INVALID_LSN};
+    lsn_t      prev_lsn_{INVALID_LSN};
+    txn_id_t   txn_id_{INVALID_TXN_ID};
+ 
+    // Insert 字段
+    KeyType ins_key_{};
+    ValType ins_val_{};
+ 
+    // Delete 字段
+    KeyType del_key_{};
+    ValType del_val_{};
+ 
+    // Update 字段
+    KeyType old_key_{};
+    ValType old_val_{};
+    KeyType new_key_{};
+    ValType new_val_{};
+ 
     /* used for testing only */
     static std::unordered_map<txn_id_t, lsn_t> prev_lsn_map_;
     static lsn_t next_lsn_;
@@ -43,45 +58,68 @@ lsn_t LogRec::next_lsn_ = 0;
 typedef std::shared_ptr<LogRec> LogRecPtr;
 
 /**
- * TODO: Student Implement
+ * 辅助函数：分配 lsn，设置 prev_lsn，并更新 prev_lsn_map_。
  */
+static void FillLsnAndPrev(LogRec &rec, txn_id_t txn_id) {
+    rec.txn_id_   = txn_id;
+    rec.lsn_      = LogRec::next_lsn_++;
+    // 若该事务之前有日志，prev_lsn 指向上一条；否则为 INVALID_LSN
+    auto it = LogRec::prev_lsn_map_.find(txn_id);
+    rec.prev_lsn_ = (it != LogRec::prev_lsn_map_.end()) ? it->second : INVALID_LSN;
+    // 更新该事务的最新 lsn
+    LogRec::prev_lsn_map_[txn_id] = rec.lsn_;
+}
+
+
 static LogRecPtr CreateInsertLog(txn_id_t txn_id, KeyType ins_key, ValType ins_val) {
-    return nullptr;
+    auto rec = std::make_shared<LogRec>();
+    rec->type_    = LogRecType::kInsert;
+    rec->ins_key_ = std::move(ins_key);
+    rec->ins_val_ = ins_val;
+    FillLsnAndPrev(*rec, txn_id);
+    return rec;
 }
 
-/**
- * TODO: Student Implement
- */
+
 static LogRecPtr CreateDeleteLog(txn_id_t txn_id, KeyType del_key, ValType del_val) {
-    return nullptr;
+    auto rec = std::make_shared<LogRec>();
+    rec->type_    = LogRecType::kDelete;
+    rec->del_key_ = std::move(del_key);
+    rec->del_val_ = del_val;
+    FillLsnAndPrev(*rec, txn_id);
+    return rec;
 }
 
-/**
- * TODO: Student Implement
- */
 static LogRecPtr CreateUpdateLog(txn_id_t txn_id, KeyType old_key, ValType old_val, KeyType new_key, ValType new_val) {
-    return nullptr;
+    auto rec = std::make_shared<LogRec>();
+    rec->type_    = LogRecType::kUpdate;
+    rec->old_key_ = std::move(old_key);
+    rec->old_val_ = old_val;
+    rec->new_key_ = std::move(new_key);
+    rec->new_val_ = new_val;
+    FillLsnAndPrev(*rec, txn_id);
+    return rec;
 }
 
-/**
- * TODO: Student Implement
- */
 static LogRecPtr CreateBeginLog(txn_id_t txn_id) {
-    return nullptr;
+    auto rec = std::make_shared<LogRec>();
+    rec->type_ = LogRecType::kBegin;
+    FillLsnAndPrev(*rec, txn_id);
+    return rec;
 }
 
-/**
- * TODO: Student Implement
- */
 static LogRecPtr CreateCommitLog(txn_id_t txn_id) {
-    return nullptr;
+    auto rec = std::make_shared<LogRec>();
+    rec->type_ = LogRecType::kCommit;
+    FillLsnAndPrev(*rec, txn_id);
+    return rec;
 }
 
-/**
- * TODO: Student Implement
- */
 static LogRecPtr CreateAbortLog(txn_id_t txn_id) {
-    return nullptr;
+    auto rec = std::make_shared<LogRec>();
+    rec->type_ = LogRecType::kAbort;
+    FillLsnAndPrev(*rec, txn_id);
+    return rec;
 }
 
 #endif  // MINISQL_LOG_REC_H
--- src/include/recovery/recovery_manager.h
+++ src/include/recovery/recovery_manager.h
@@ -23,19 +23,62 @@ struct CheckPoint {
 class RecoveryManager {
 public:
     /**
-    * TODO: Student Implement
+    * DONE: 从 checkpoint 恢复数据库状态和活跃事务表。
     */
-    void Init(CheckPoint &last_checkpoint) {}
+    void Init(CheckPoint &last_checkpoint) {
+        persist_lsn_  = last_checkpoint.checkpoint_lsn_;
+        data_         = last_checkpoint.persist_data_;
+        active_txns_  = last_checkpoint.active_txns_;
+    }
 
     /**
-    * TODO: Student Implement
+    * DONE: 从 persist_lsn_ 之后，按 lsn 顺序重放所有日志
     */
-    void RedoPhase() {}
-
+    void RedoPhase() {
+        for (auto &[lsn, rec] : log_recs_) {
+            if (lsn <= persist_lsn_) continue;
+ 
+            switch (rec->type_) {
+                case LogRecType::kInsert:
+                    data_[rec->ins_key_] = rec->ins_val_;
+                    active_txns_[rec->txn_id_] = lsn;
+                    break;
+                case LogRecType::kDelete:
+                    // Redo Delete：写入被删的旧值（Undo 阶段才决定是否恢复）
+                    data_[rec->del_key_] = rec->del_val_;
+                    active_txns_[rec->txn_id_] = lsn;
+                    break;
+                case LogRecType::kUpdate:
+                    data_[rec->new_key_] = rec->new_val_;
+                    active_txns_[rec->txn_id_] = lsn;
+                    break;
+                case LogRecType::kBegin:
+                    if (active_txns_.find(rec->txn_id_) == active_txns_.end()) {
+                        active_txns_[rec->txn_id_] = lsn;
+                    }
+                    break;
+                case LogRecType::kCommit:
+                    active_txns_.erase(rec->txn_id_);
+                    break;
+                case LogRecType::kAbort:
+                    // Abort：立即回滚该事务（包含 checkpoint 之前的日志）
+                    UndoTxn(rec->prev_lsn_);
+                    active_txns_.erase(rec->txn_id_);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
     /**
-    * TODO: Student Implement
+    * DONE: 对 active_txns_ 中仍活跃的事务（未 Commit/Abort）逐一回滚
     */
-    void UndoPhase() {}
+    void UndoPhase() {
+        for (auto &[txn_id, last_lsn] : active_txns_) {
+            UndoTxn(last_lsn);
+        }
+        active_txns_.clear();
+    }
 
     // used for test only
     void AppendLogRec(LogRecPtr log_rec) { log_recs_.emplace(log_rec->lsn_, log_rec); }
@@ -44,6 +87,32 @@ public:
     inline KvDatabase &GetDatabase() { return data_; }
 
 private:
+    /**
+     * 辅助函数：沿 prev_lsn 链回滚一个事务的所有日志。
+     * @param last_lsn 该事务最后一条需要 Undo 的日志的 lsn
+     */
+    void UndoTxn(lsn_t last_lsn) {
+        lsn_t cur = last_lsn;
+        while (cur != INVALID_LSN) {
+            auto it = log_recs_.find(cur);
+            if (it == log_recs_.end()) break;
+            auto &rec = it->second;
+            switch (rec->type_) {
+                case LogRecType::kInsert:
+                    data_.erase(rec->ins_key_);
+                    break;
+                case LogRecType::kDelete:
+                    data_[rec->del_key_] = rec->del_val_;
+                    break;
+                case LogRecType::kUpdate:
+                    data_[rec->old_key_] = rec->old_val_;
+                    break;
+                default:
+                    break;
+            }
+            cur = rec->prev_lsn_;
+        }
+    }
     std::map<lsn_t, LogRecPtr> log_recs_{};
     lsn_t persist_lsn_{INVALID_LSN};
     ATT active_txns_{};
